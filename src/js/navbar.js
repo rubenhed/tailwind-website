edd9a1c7c033b75d328c0205e0f7cc21143b22fc
Ruben Hedstrom/ルーベン・ヘドストロム <ruben.hedstrom@gadelius.com>
2025-01-08 16:28:14 +0900

    added hover

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,10 +1,47 @@
+TIMEOUT = 100;
 
 //hamburger menu
 document.getElementById("mobile-menu").addEventListener("click", () => {
   document.getElementById("nav-menu").classList.toggle("hidden");
 })
 
-//dropdown
+//dropdown hover
+document.querySelectorAll(".dropdown-toggle").forEach((toggleMenu) => {
+  const dropdownMenu = toggleMenu.nextElementSibling;
+  let hideTimeout;
+
+  toggleMenu.addEventListener("mouseenter", () => {
+    if (window.innerWidth > 768) {
+      toggleMenu.querySelector("img").classList.add("rotate-180");
+      dropdownMenu.classList.remove("hidden");
+    };
+  });
+
+  toggleMenu.addEventListener("mouseleave", () => {
+    if (window.innerWidth > 768) {
+      hideTimeout = setTimeout(() => {
+        toggleMenu.querySelector("img").classList.remove("rotate-180");
+        dropdownMenu.classList.add("hidden");
+      }, TIMEOUT);
+    };
+  });
+
+  dropdownMenu.addEventListener("mouseenter", () => {
+    clearTimeout(hideTimeout);
+  });
+
+  dropdownMenu.addEventListener("mouseleave", () => {
+    if (window.innerWidth > 768) {
+      hideTimeout = setTimeout(() => {
+        toggleMenu.querySelector("img").classList.remove("rotate-180");
+        dropdownMenu.classList.add("hidden");
+      }, TIMEOUT);
+    };
+  });
+});
+
+
+//dropdown click
 window.addEventListener("click", (event) => {
   const hidden = event.target.classList.contains("dropdown-toggle") && event.target.nextElementSibling.classList.contains("hidden");
 
