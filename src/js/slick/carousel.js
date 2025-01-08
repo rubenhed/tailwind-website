846bdd59068b0ef2229c01cef5d115296d57d78a
Ruben Hedstrom/ルーベン・ヘドストロム <ruben.hedstrom@gadelius.com>
2025-01-08 16:10:31 +0900

    s

--- src/js/slick/carousel.js
+++ src/js/slick/carousel.js
@@ -2,7 +2,7 @@
 $(window).on('load resize', () => {
   const ww = $(window).width();
   const sw = 1000;
-  const centerPadding = Math.max((ww - sw) / 2, 60) + 'px';
+  const centerPadding = Math.max((ww - sw) / 2, 50) + 'px';
   $('.slider').slick('slickSetOption', {
     centerPadding: centerPadding,
   });
